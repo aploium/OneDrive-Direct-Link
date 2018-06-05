caedd4001c26b00e7b11a10144ee2f333c27a5d2
aploium <i@z.codes>
2018-06-05 22:26:02 +0800

    1. fix /w/s /u/s /t/s share url bug.  2. add get text mode  2. add cache

--- app.py
+++ app.py
@@ -1,25 +1,36 @@
 # coding=utf-8
 import requests
-from flask import Flask, Response
+from flask import Flask, request, redirect
+from cachetools.func import ttl_cache
 
 app = Flask(__name__)
 
 
-@app.route('/t/s!<ts_token>')
-def share_ts(ts_token):
-    session = requests.Session()
-    url_step1 = 'https://1drv.ms/t/s!' + ts_token
-    resp_step1 = session.get(url_step1, timeout=10, allow_redirects=False)
+@ttl_cache(1024, ttl=600)
+def get_direct_url(ch, share_token):
+    url_step1 = 'https://1drv.ms/{ch}/s!{share_token}'.format(
+        ch=ch,  # the ch could be w u t or more
+        share_token=share_token
+    )
+    resp_step1 = requests.get(url_step1, timeout=10, allow_redirects=False)
     url_step2 = resp_step1.headers['Location']
     url_step3 = url_step2.replace('/redir?', '/download?')
-    resp_step3 = session.get(url_step3, timeout=10, allow_redirects=False)
+    resp_step3 = requests.get(url_step3, timeout=10, allow_redirects=False)
     url_final = resp_step3.headers['Location']
 
-    return Response(url_final,
-                    status=302,
-                    headers={'Location': url_final},
-                    mimetype='text/plain',
-                    )
+    return url_final
+
+
+@app.route('/<ch>/s!<share_token>')
+def share_ts(ch, share_token):
+    url_direct = get_direct_url(ch, share_token)
+
+    if 'txt' in request.args:
+        # display plain text
+        return url_direct
+    else:
+        # 301 redirect
+        return redirect(url_direct, code=301)
 
 
 @app.route('/')
@@ -35,10 +46,13 @@ def index():
 <h1>The OneDrive Direct Download Link Helper</h1>
 <p>Get OneDrive direct download link by just changing the "1drv.ms" to "1drv.ws"</p>
 <p>Example: <br>
-1. Get the share link, like this: https://1drv.ms/t/s!sOme1Ra4dCh1r_aB (fake link)<br>
+1. Get the share link, like this: <code>https://1drv.ms/t/s!sOme1Ra4dCh1r_aB</code> (fake link)<br>
 2. Change the domain <b>1drv.ms</b> to <b>1drv.ws</b>, I mean, just flip the <b>m</b> to <b>w</b><br>
-which becomes https://1drv.<b>w</b>s/t/s!sOme1Ra4dCh1r_aB<br>
-3. This IS the direct link, you can paste it to browser and see.<br>
+which becomes <code>https://1drv.<b>w</b>s/t/s!sOme1Ra4dCh1r_aB</code><br>
+3. This <b>IS</b> the direct link, you can paste it to browser and see.<br>
+</p>
+<p>btw, you can add <code>?txt</code> at the end of url, to display link in page, instead of 301 redirect to it.<br>
+eg: <code>https://1drv.<b>w</b>s/t/s!sOme1Ra4dCh1r_aB?<b>txt</b></code>
 </p>
 <hr>
 <h2>How it works</h2>
